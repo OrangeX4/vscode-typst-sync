498322f88e132e315930b8a1227b9ca1bc1691e4
sergiofang <sergiofang@tencent.com>
2023-10-10 13:12:57 +0800

    init

---
+++ src/extension.ts
@@ -0,0 +1,35 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { window, commands, ExtensionContext } from 'vscode';
+import { showQuickPick, showInputBox } from './basicInput';
+import { multiStepInput } from './multiStepInput';
+import { quickOpen } from './quickOpen';
+
+export function activate(context: ExtensionContext) {
+	context.subscriptions.push(commands.registerCommand('typst-sync.quickInput', async () => {
+		const options: { [key: string]: (context: ExtensionContext) => Promise<void> } = {
+			showQuickPick,
+			showInputBox,
+			multiStepInput,
+			quickOpen,
+		};
+		const quickPick = window.createQuickPick();
+		quickPick.items = Object.keys(options).map(label => ({ label }));
+		quickPick.onDidChangeSelection(selection => {
+			if (selection[0]) {
+				options[selection[0].label](context)
+					.catch(console.error);
+			}
+		});
+		quickPick.onDidHide(() => quickPick.dispose());
+		quickPick.show();
+	}));
+
+  // Create a new typst local package
+	context.subscriptions.push(commands.registerCommand('typst-sync.createLocalPackage', async () => {
+    multiStepInput(context);
+	}));
+}
